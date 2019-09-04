e1a075e02bd743fbd05ac79359ef1052a2233f72
carlodeba <54215267+carlodeba@users.noreply.github.com>
2019-09-04 18:17:23 +0200

    Create backup_mysql.sh

---
+++ backup_mysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DATE=$(/bin/date +%a-%H)
+SQL_FILES=/var/backup/mysql_all_$DATE
+MYSQL='mysql -N -uroot -p$MYSQL_ROOT_PASSWORD'
+MYSQLDUMP='mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --single-transaction'
+DATABASES=`echo show databases|${MYSQL}|xargs`
+mkdir -v  ${SQL_FILES}
+for db in $DATABASES
+do
+        nice -n 19 ${MYSQLDUMP} ${db} > $SQL_FILES/$db.sql
+done
+nice -n 19 tar cjvpf ${SQL_FILES}.tar.bz2 ${SQL_FILES} && rm -fr ${SQL_FILES}
